7ce17fc31141e5fd78712e6e6831c4d98f684535
sahil khan <thesahindia@gmail.com>
2021-10-24 13:08:53 +0530

    setup cart item count

--- src/components/cart/CartIcon.js
+++ src/components/cart/CartIcon.js
@@ -1,18 +1,29 @@
-import React from "react";
 import { ReactComponent as ShoppingBag } from "../../assets/images/bag.svg";
 import "./cartIcon.scss";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { toggleCartHidden } from "../../redux/actions";
 
 const CartIcon = () => {
+  const cartItems = useSelector(({ cart: { cartItems } }) => cartItems);
+
+  const cartItemCount = () => {
+    if (cartItems.length) {
+      return cartItems
+        .map(({ quantity }) => quantity)
+        .reduce((acc, currnt) => acc + currnt);
+    }
+    return 0;
+  };
   const dispatch = useDispatch();
+
   const handleClick = () => {
     dispatch(toggleCartHidden());
   };
+  
   return (
     <div className="cart-icon" onClick={handleClick}>
       <ShoppingBag />
-      <span className="item-count"></span>
+      <span className="item-count">{cartItemCount()}</span>
     </div>
   );
 };
